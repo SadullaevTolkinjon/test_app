1656343b9955b79f095d117b2da16da5ef288472
SadullaevTolkinjon <sadullayevtolik@gmail.com>
2022-03-08 23:02:30 +0500

    Updated

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:test_zadaniya/core/size_config/size_config.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-    
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+        ],
+      )),
     );
   }
 }
--- lib/service/my_service.dart
+++ lib/service/my_service.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-class UserService{
+import 'package:test_zadaniya/model/user_model.dart';
 
+class UserService {
+  static Future<List<UserModel>> getUserService() async {
+    Uri url = Uri.parse("https://jsonplaceholder.typicode.com/users");
+    dynamic response = await http.get(url);
+    response = jsonDecode(response.body);
+    return (response as List).map((e) => UserModel.fromJson(e)).toList();
+  }
 }
